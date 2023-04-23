e369fcbf60bcbcbc0ec0883abce086643534e19c
Daniel Paiva Filho <danielpaivaf11@gmail.com>
2023-04-23 20:12:17 -0300

    Enviando o primeiro commit ao github

---
+++ novoProjeto/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$meses = array('Janeiro', 'Fevereiro');
+
+foreach ($meses as $index => $mes) {
+	// code...
+	echo "Indice: ".$index. "<br>";
+	echo "O mes e: " . $mes."<br><br>";
+}
+
+
+?>
